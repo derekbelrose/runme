aca2b64cbd58a8963571bf288f301cc39524fe1c
Derek Belrose <derek@derekbelrose.com>
2023-05-31 15:20:51 -0400

    Testing

--- flake.nix
+++ flake.nix
@@ -1,22 +1,29 @@
 {
-	description = "Test build environmnent";
-	inputs = { 
-		nixpkgs.url = "github:nixos/nixpkgs/22.11"; 
-		flake-utils.url = "github:numtide/flake-utils";
-	};
+  description = "runme application";
 
-	outputs = { self, nixpkgs, flake-utils }:
-	  flake-utils.lib.eachDefaultSystem(system:
-		let
-			pkgs = import nixpkgs { inherit system; };
-		in {
-			packages.default = pkgs.writeScriptBin "runme" ''
-				echo "Beep beep!"
-			'';
+  inputs = {
+    nixpkgs.url = "nixpkgs"; # Resolves to github:NixOS/nixpkgs
+    # Helpers for system-specific outputs
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-			apps.default = {
-				type = "app";
-				program = "${self.packages.${system}.runme}/bin/runme";
-			};
-		});
+  outputs = { self, nixpkgs, flake-utils }:
+    # Create system-specific outputs for the standard Nix systems
+    # https://github.com/numtide/flake-utils/blob/master/default.nix#L3-L9
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+      	pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # A simple executable package
+        packages.default = pkgs.writeScriptBin "runme" ''
+          echo "I am currently being run!"
+        '';
+
+        # An app that uses the `runme` package
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.runme}/bin/runme";
+        };
+      });
 }
